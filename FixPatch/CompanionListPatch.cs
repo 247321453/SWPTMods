a40670f446e8de81d541bb4db74cdade912e905c
kenan <247321453@qq.com>
2021-08-01 16:51:26 +0800

    支持保存组队名单

--- FixPatch/CompanionListPatch.cs
+++ FixPatch/CompanionListPatch.cs
@@ -15,7 +15,9 @@ namespace FixPatch
 {
     class CompanionListPatch
     {
+        public static string CompanionListKey = "Haibao.txt?tag=lastpartylist";
         public static bool UIInventoryIsOpen = false;
+        public static readonly List<string> lastPartys = new List<string>();
 
         public static void OnChangeToLeft()
         {
@@ -89,6 +91,117 @@ namespace FixPatch
         private static void Postfix(UIInventory __instance)
         {
             CompanionListPatch.UIInventoryIsOpen = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Scene), "Start")]
+    class Scene_Start_Patch
+    {
+        private static void Postfix(Scene __instance)
+        {
+            if (BepInExPlugin.isCompanionSort.Value)
+            {
+                if (Global.code.curlocation && Global.code.curlocation.locationType == LocationType.home)
+                {
+                    return;
+                }
+                if (Global.code.playerCombatParty.items.Count > 0)
+                {
+                    CompanionListPatch.lastPartys.Clear();
+                    foreach (Transform item in Global.code.playerCombatParty.items)
+                    {
+                        if (item)
+                        {
+                            CompanionListPatch.lastPartys.Add(item.name);
+                        }
+                    }
+                    // string id = Mainframe.code.GetFolderName() + CompanionListPatch.CompanionListKey;
+                    BepInExPlugin.Debug(Mainframe.code.GetFolderName() + " set last partys=" + string.Join(",", CompanionListPatch.lastPartys));
+                    // ES2.Save<string>(CompanionListPatch.lastPartys, id);
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(Global), "AddCompanionToPlayerArmy")]
+    class Global_AddCompanionToPlayerArmy_Patch
+    {
+        private static bool Prefix(Global __instance, Transform companion)
+        {
+            if (!BepInExPlugin.isCompanionSort.Value)
+            {
+                return true;
+            }
+            __instance.companions.AddItem(companion);
+            companion.transform.SetParent(__instance.transform);
+            __instance.playerCombatParty.ArrangeItems();
+            if (CompanionListPatch.lastPartys.Contains(companion.name))
+            {
+                //TODO 限制数量？
+                if (!Global.code.playerCombatParty.items.Exists(t => t.name == companion.name))
+                {
+                    __instance.playerCombatParty.AddItemDifferentObject(companion);
+                }
+            }
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Mainframe), "LoadCompanions")]
+    class Mainframe_LoadCompanions_Patch
+    {
+        private static void Prefix(Mainframe __instance)
+        {
+            if (BepInExPlugin.isCompanionSort.Value)
+            {
+                string id = __instance.GetFolderName() + CompanionListPatch.CompanionListKey;
+                if (ES2.Exists(id))
+                {
+                    CompanionListPatch.lastPartys.Clear();
+                    CompanionListPatch.lastPartys.AddRange(ES2.LoadList<string>(id));
+                    BepInExPlugin.Debug(__instance.GetFolderName() + " read last partys=" + string.Join(",", CompanionListPatch.lastPartys));
+                    /*
+                     if (CompanionListPatch.lastPartys.Count > 0)
+                     {
+                         BepInExPlugin.Debug(__instance.GetFolderName() + " all companions="+ Global.code.companions.items.Count);
+                         Global.code.playerCombatParty.ClearItems();
+                         foreach (var name in CompanionListPatch.lastPartys)
+                         {
+                             var item = Global.code.companions.GetItemWithName(name);
+                             if (item)
+                             {
+                                 Global.code.playerCombatParty.AddItemDifferentObject(item);
+                             }
+                         }
+                         BepInExPlugin.Debug(__instance.GetFolderName() + " read last partys=" + string.Join(",", CompanionListPatch.lastPartys) + ", size=" + Global.code.playerCombatParty.items.Count);
+                         if (Global.code.playerCombatParty.items.Count == 0) {
+                             foreach (var item in Global.code.companions.items)
+                             {
+                                 if (item)
+                                 {
+                                     BepInExPlugin.Debug(__instance.GetFolderName() + " companions item=" + item.name);
+                                 }
+                             }
+                         }
+                     }*/
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Mainframe), "SaveGame")]
+    class Mainframe_SaveGame_Patch
+    {
+        private static void Postfix(Mainframe __instance)
+        {
+            if (BepInExPlugin.isCompanionSort.Value)
+            {
+                if (Global.code.curlocation && Global.code.curlocation.locationType == LocationType.home)
+                {
+                    string id = Mainframe.code.GetFolderName() + CompanionListPatch.CompanionListKey;
+                    BepInExPlugin.Debug(Mainframe.code.GetFolderName() + " save last partys=" + string.Join(",", CompanionListPatch.lastPartys));
+                    ES2.Save<string>(CompanionListPatch.lastPartys, id);
+                }
+            }
         }
     }
 }
